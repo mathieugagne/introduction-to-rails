26c113e6c4c2105366cb72c375a79526ad6760c3
Mathieu Gagne <gagne.mathieu@hotmail.com>
2014-04-23 10:02:03 -0400

    Add remaining navigation links

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
 
   get '/getting-started', to: 'steps#getting_started'
+  get '/add-project-to-git', to: 'steps#add_project_to_git'
+  get '/running-application', to: 'steps#running_application'
+  get '/creating-migration', to: 'steps#creating_migration'
+  get '/rails-architecture', to: 'steps#rails_architecture'
+  get '/crud-scaffolding', to: 'steps#crud_scaffolding'
+  get '/setting-default-page', to: 'steps#setting_default_page'
+  get '/voting-topics', to: 'steps#voting_topics'
+  get '/votes-topics-association', to: 'steps#votes_topics_association'
+  get '/allow-people-to-vote', to: 'steps#allow_people_to_vote'
+  get '/redirect-topics-after-create', to: 'steps#redirect_topics_after_create'
+  get '/topic-title-link', to: 'steps#topic_title_link'
+  get '/clean-up-links', to: 'steps#clean_up_links'
+  get '/credits', to: 'steps#credits'
 
   root to: 'steps#overview'
 end
