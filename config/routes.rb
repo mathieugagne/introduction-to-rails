8d4a2b01cc3f112115939ee6ae5d5afb04d11bb2
Mathieu Gagne <gagne.mathieu@hotmail.com>
2014-04-23 17:11:36 -0400

    Add install instructions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get '/install-instructions', to: 'steps#install_instructions'
   get '/getting-started', to: 'steps#getting_started'
   get '/add-project-to-git', to: 'steps#add_project_to_git'
   get '/running-application', to: 'steps#running_application'
